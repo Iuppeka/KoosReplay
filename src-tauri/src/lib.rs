354a3b0efae2ccee3cfdf94805c8713a8c81ef43
Luppeka <kooshendrikrijskamp@gmail.com>
2026-08-23 22:59:09 +0200

    Add capture module and manage CaptureState

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,18 @@
+mod capture;
+
+use capture::CaptureState;
+use std::sync::Mutex;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(CaptureState {
+            process: Mutex::new(None),
+        })
+        .invoke_handler(tauri::generate_handler![
+            capture::start_capture,
+            capture::stop_capture
+        ])
         .run(tauri::generate_context!())
         .expect("error while running KoosReplay");
 }
