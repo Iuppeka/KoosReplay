86dfb8ca7342c80b24730096d877bb56f6a0d59c
Luppeka <kooshendrikrijskamp@gmail.com>
2026-08-24 22:30:53 +0200

    Refactor capture module imports and manage CaptureState

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,26 +1,42 @@
-mod capture;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use capture::CaptureState;
-use std::sync::Mutex;
+mod capture;
+mod replay_buffer;
+
+use capture::{
+    save_replay,
+    start_capture,
+    stop_capture,
+    CaptureState,
+};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        /*
+         * Capture state
+         */
         .manage(CaptureState {
-            process: Mutex::new(None),
-            buffer_dir: Mutex::new(None),
+            process: std::sync::Mutex::new(None),
+            buffer_dir: std::sync::Mutex::new(None),
         })
-        .plugin(
-            tauri_plugin_global_shortcut::Builder::new()
-                .build(),
-        )
+
+        /*
+         * Tauri commands available to App.tsx
+         */
         .invoke_handler(
             tauri::generate_handler![
-                capture::start_capture,
-                capture::stop_capture,
-                capture::save_replay
-            ],
+                start_capture,
+                stop_capture,
+                save_replay
+            ]
         )
+
+        /*
+         * Start the application.
+         */
         .run(tauri::generate_context!())
-        .expect("error while running KoosReplay");
+        .expect(
+            "error while running KoosReplay"
+        );
 }
